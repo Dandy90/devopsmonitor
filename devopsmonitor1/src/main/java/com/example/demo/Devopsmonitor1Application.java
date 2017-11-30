40de812079f9fcb5ae274a0d567f0805cdb45ea1
Igor Romanko <igor.romanko@gmail.com>
2017-11-30 21:13:43 +0100

    Project base setup
    Change-Id: I56d8e96b0082211b628d635a63ba1ca6b8a20f96

---
+++ devopsmonitor1/src/main/java/com/example/demo/Devopsmonitor1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopsmonitor1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devopsmonitor1Application.class, args);
+	}
+}
